ae8669a31384589b0c812a387eccbf1c334537de
Kushagra Gupta <kushagragupta853@gmail.com>
2025-02-25 16:00:30 +0530

    [Kushagra] Update. Employee DTO program in UC6

--- src/main/java/com/example/EmployeePayroll/dto/EmployeeDTO.java
+++ src/main/java/com/example/EmployeePayroll/dto/EmployeeDTO.java
@@ -1,42 +1,15 @@
 package com.example.EmployeePayroll.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class EmployeeDTO {
 
     private String name;
     private double salary;
     private String department;
-
-    // Constructors
-    public EmployeeDTO() {}
-
-    public EmployeeDTO(String name, double salary, String department) {
-        this.name = name;
-        this.salary = salary;
-        this.department = department;
-    }
-
-    // Getters and Setters
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public double getSalary() {
-        return salary;
-    }
-
-    public void setSalary(double salary) {
-        this.salary = salary;
-    }
-
-    public String getDepartment() {
-        return department;
-    }
-
-    public void setDepartment(String department) {
-        this.department = department;
-    }
 }
