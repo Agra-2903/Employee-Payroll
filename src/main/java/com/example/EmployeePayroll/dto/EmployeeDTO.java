4f80eb55e0027b0dc85bddb55948f3dbd6cb6c01
Kushagra Gupta <kushagragupta853@gmail.com>
2025-03-01 16:22:54 +0530

    [Kushagra] Update. Employee DTO program in UC10

--- src/main/java/com/example/EmployeePayroll/dto/EmployeeDTO.java
+++ src/main/java/com/example/EmployeePayroll/dto/EmployeeDTO.java
@@ -1,5 +1,7 @@
 package com.example.EmployeePayroll.dto;
 
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -9,7 +11,10 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class EmployeeDTO {
 
+    @NotEmpty(message = "Name is required and cannot be empty")
+    @Pattern(regexp = "^[A-Z][a-zA-Z\\s]{2,}$", message = "Name must start with a capital letter and have at least 3 characters")
     private String name;
+
     private double salary;
     private String department;
 }
